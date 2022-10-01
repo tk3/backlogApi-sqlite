b6681d65f72f7fb617c79947f4873f4dcf38aa18
tk3 <tk3inc@gmail.com>
2022-10-01 14:57:46 +0000

    update

--- main.rb
+++ main.rb
@@ -5,51 +5,21 @@ require 'sqlite3'
 
 module Blg
   class Query
-    class Context
-      class Project
-        attr_reader :id_or_key
+    def self.context(&block)
+      Blg::Context.new(&block)
+    end
+  end
 
-        def initialize(id_or_key, options = {})
-          @id_or_key = id_or_key
-          @api = Blg::Client.new(
-            options[:endpoint],
-            options[:api_key]
-          )
-          @db = SQLite3::Database.new ':memory:'
+  class Schema
+    def self.create_table(db)
+      db.execute(issues_table)
+      db.execute(users_table)
+      db.execute(statuses_table)
+      db.execute(issue_types_table)
+    end
 
-          yield(self) if block_given?
-        end
-
-        def fetch(name, source)
-          case name
-          when :issue_types
-            fetch_issue_types(source)
-          when :statuses
-            fetch_statuses(source)
-          when :issues
-            fetch_issues(source)
-          end
-        end
-
-        def execute(*bind_vars, &block)
-          @db.execute(*bind_vars, &block)
-        end
-
-        private
-
-        def table_exists(table_name)
-          rows = @db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name=?", table_name)
-          rows[0][0] == 1
-        end
-
-        def record_exists(table_name, id)
-          rows = @db.execute("SELECT count(*) FROM #{table_name} WHERE id=?", id)  # TODO:
-          rows[0][0] == 1
-        end
-
-        def fetch_issues(issues)
-          unless table_exists('issue')
-            rows = @db.execute <<-SQL
+    def self.issues_table
+<<-SQL
 CREATE TABLE issues (
   id            integer unique,
   summary       text,
@@ -58,82 +28,112 @@ CREATE TABLE issues (
   assignee_id   integer
 );
 SQL
-          end
+    end
 
-          issues.each do |issue|
-            if !record_exists('issues', issue['id'])
-              assignee = if issue['assignee'] == nil
-                           {'id' => nil}
-                         else
-                           issue['assignee']
-                         end
-              @db.execute 'INSERT INTO issues VALUES (?, ?, ?, ?, ?)',
-                [
-                  issue['id'],
-                  issue['summary'],
-                  issue['issueType']['id'],
-                  issue['status']['id'],
-                  assignee['id']
-                ]
-
-              unless assignee['id'] == nil
-                unless table_exists('users')
-            rows = @db.execute <<-SQL
+    def self.users_table
+<<-SQL
 CREATE TABLE users (
   id integer unique,
   name text
 );
 SQL
-                end
-                if !record_exists('users', assignee['id'])
-                  @db.execute 'INSERT INTO users VALUES (?, ?)', [assignee['id'], assignee['name']]
-                end
-              end
-            end
-          end
-        end
+    end
 
-        def fetch_statuses(statuses)
-          unless table_exists('statuses')
-            rows = @db.execute <<-SQL
+    def self.statuses_table
+<<-SQL
 CREATE TABLE statuses (
   id integer unique,
   name text
 );
 SQL
-          end
+    end
 
-          statuses.each do |status|
-            if !record_exists('statuses', status['id'])
-              @db.execute 'INSERT INTO statuses VALUES (?, ?)', [status['id'], status['name']]
-            end
-          end
-        end
-
-        def fetch_issue_types(issue_types)
-
-          unless table_exists('issue_types')
-            rows = @db.execute <<-SQL
+    def self.issue_types_table
+<<-SQL
 CREATE TABLE issue_types (
   id integer unique,
   name text
 );
 SQL
-          end
+    end
+  end
 
-          issue_types.each do |issue_type|
-            if !record_exists('issue_types', issue_type['id'])
-              @db.execute 'INSERT INTO issue_types VALUES (?, ?)', [issue_type['id'], issue_type['name']]
+  class Context
+    def initialize
+      @db = SQLite3::Database.new ':memory:'
+      Schema.create_table(@db)
+ 
+      yield(self) if block_given?
+    end
+
+    def fetch(name, source)
+      case name
+      when :issue_types
+        fetch_issue_types(source)
+      when :statuses
+        fetch_statuses(source)
+      when :issues
+        fetch_issues(source)
+      end
+    end
+
+    def execute(*bind_vars, &block)
+      @db.execute(*bind_vars, &block)
+    end
+
+    private
+
+    def table_exists(table_name)
+      rows = @db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name=?", table_name)
+      rows[0][0] == 1
+    end
+
+    def record_exists(table_name, id)
+      rows = @db.execute("SELECT count(*) FROM #{table_name} WHERE id=?", id)  # TODO:
+      rows[0][0] == 1
+    end
+
+    def fetch_issues(issues)
+      issues.each do |issue|
+        if !record_exists('issues', issue['id'])
+          assignee = if issue['assignee'] == nil
+                       {'id' => nil}
+                     else
+                       issue['assignee']
+                     end
+          @db.execute 'INSERT INTO issues VALUES (?, ?, ?, ?, ?)',
+            [
+              issue['id'],
+              issue['summary'],
+              issue['issueType']['id'],
+              issue['status']['id'],
+              assignee['id']
+            ]
+
+          unless assignee['id'] == nil
+            if !record_exists('users', assignee['id'])
+              @db.execute 'INSERT INTO users VALUES (?, ?)', [assignee['id'], assignee['name']]
             end
           end
         end
       end
     end
 
-    def self.project(id_or_key, api, &block)
-      Blg::Query::Context::Project.new(id_or_key, api, &block)
+    def fetch_statuses(statuses)
+      statuses.each do |status|
+        if !record_exists('statuses', status['id'])
+          @db.execute 'INSERT INTO statuses VALUES (?, ?)', [status['id'], status['name']]
+        end
+      end
     end
 
+    def fetch_issue_types(issue_types)
+      issue_types.each do |issue_type|
+        if !record_exists('issue_types', issue_type['id'])
+          @db.execute 'INSERT INTO issue_types VALUES (?, ?)', [issue_type['id'], issue_type['name']]
+        end
+      end
+    end
   end
 end
 
@@ -147,25 +147,26 @@ api = Blg::Client.new(endpoint, api_key)
 
 projects = api.projects
 
-Blg::Query::project(projects[0]['id'], {:endpoint => endpoint, :api_key => api_key}) do |project|
-  puts project.id_or_key
-  project.fetch(:issue_types, api.issueTypes(project.id_or_key))
-  project.fetch(:statuses, api.statuses(project.id_or_key))
-  project.fetch(:issues, api.issues({'count' => 100}))
+project_id = projects[0]['id']
 
-  project.execute('SELECT * FROM issue_types') do |row|
+Blg::Query.context do |ctx|
+  ctx.fetch(:issue_types, api.issueTypes(project_id))
+  ctx.fetch(:statuses, api.statuses(project_id))
+  ctx.fetch(:issues, api.issues({'count' => 100}))
+
+  ctx.execute('SELECT * FROM issue_types') do |row|
     p row
   end
 
-  project.execute('SELECT * FROM statuses') do |row|
+  ctx.execute('SELECT * FROM statuses') do |row|
     p row
   end
 
-  project.execute('SELECT * FROM issues') do |row|
+  ctx.execute('SELECT * FROM issues') do |row|
     p row
   end
 
-  project.execute('SELECT * FROM users') do |row|
+  ctx.execute('SELECT * FROM users') do |row|
     p row
   end
 
@@ -176,7 +177,7 @@ INNER JOIN users u
 ON i.assignee_id  = u.id
 GROUP BY u.id
 SQL
-  project.execute(sql) do |row|
+  ctx.execute(sql) do |row|
     p row
   end
 end
