a58b19644b835d8563f95d8c7e49bcc7552499f9
tk3 <tk3inc@gmail.com>
2022-11-09 19:58:12 +0900

    add file

---
+++ foo.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require_relative 'backlog'
+require_relative 'main'
+
+require 'dotenv'
+Dotenv.load
+
+endpoint = ENV['BACKLOG_API_ENDPOINT']
+api_key = ENV['BACKLOG_API_KEY']
+
+api = Backlog::Client.new(endpoint, api_key)
+project_id = api.projects.filter { |project| project['name'] == 'main' }.first['id']
+
+Backlog::Query.endpoint = endpoint
+Backlog::Query.api_key = api_key
+
+Backlog::Query.context "./sample.db" do |ctx|
+  ctx.project_id = project_id
+
+  ctx.fetch_issue_types
+  ctx.fetch_statuses
+  ctx.fetch_issues {'count' => 100}
+
+end
+
